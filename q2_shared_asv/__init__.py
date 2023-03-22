c4e1a213972d5683e669d3d08e301366ee051f25
RN_ryo <57587691+geedrn@users.noreply.github.com>
2023-03-22 13:11:49 +0900

    Updated to v0.2.0

--- q2_shared_asv/__init__.py
+++ q2_shared_asv/__init__.py
@@ -1,5 +1,5 @@
 import importlib
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 importlib.import_module('q2_shared_asv.compute')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='q2-shared-asv',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     entry_points={
         'qiime2.plugins': ['q2-shared-asv=q2_shared_asv.plugin_setup:plugin']
